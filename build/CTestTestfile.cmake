08b080897b4057cf7f41a360771ba10f61fcfe93
glhwork <1584037290@qq.com>
2018-08-27 15:15:02 +0800

    delete 'build'directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/glh/renov_ws/src/renov_localization
-# Build directory: /home/glh/renov_ws/src/renov_localization/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/glh/renov_ws/src/renov_localization/include".split(';') if "/home/glh/renov_ws/src/renov_localization/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lrenov_localization".split(';') if "-lrenov_localization" != "" else []
-PROJECT_NAME = "renov_localization"
-PROJECT_SPACE_DIR = "/home/glh/renov_ws/src/renov_localization/build/devel"
-PROJECT_VERSION = "0.0.0"
